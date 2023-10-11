5dcbde8f0147ad8f6b002d4b80106723dc3aed86
Krideinside <kride2009@yandex.ru>
2023-10-11 17:38:53 +0300

    Objects fixed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,10 +10,9 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marksToAdd) {
-	if (!this.marks) {
-		this.marks = [];
-	}
+	if (this.marks) {
 	this.marks.push(...marksToAdd);
+	}
 }
 
 Student.prototype.getAverage = function() {
